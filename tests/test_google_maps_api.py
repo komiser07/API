56037546e56d26af7adaad39ce5352fbc69ae348
Mihail Komiser <komiser@gmail.com>
2024-11-10 15:47:41 +0300

    Создание кастомных методов

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,6 +1,5 @@
 from requests import Response
 from utils.checking import Checking
-
 from utils.api import GoogleMapsApi
 
 
@@ -10,27 +9,35 @@ class TestCreatePlace:
         print("\nметод POST")
         result_post: Response = GoogleMapsApi.create_new_place()
         Checking.check_status_code(result_post, 200)
+        Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
 
         print("\nметод GET POST")
         result_get: Response = GoogleMapsApi.get_new_place(result_post.json()["place_id"])
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get,
+                                  ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
+                                   'language'])
 
         print("\nметод PUT")
         result_put: Response = GoogleMapsApi.put_new_place(result_post.json()["place_id"])
         Checking.check_status_code(result_put, 200)
-
-        check_response_put = result_put.json()
-        msg = check_response_put.get('msg')
+        Checking.check_json_token(result_put, ['msg'])
 
         print("\nметод GET PUT")
         result_get: Response = GoogleMapsApi.get_new_place(result_post.json()["place_id"])
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get,
+                                  ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
+                                   'language'])
 
         print("\nметод DELETE")
         result_delete: Response = GoogleMapsApi.delete_new_place(result_post.json()["place_id"])
         Checking.check_status_code(result_delete, 200)
+        Checking.check_json_token(result_delete, ['status'])
 
         print("\nметод GET DELETE")
         result_get: Response = GoogleMapsApi.get_new_place(result_post.json()["place_id"])
         Checking.check_status_code(result_get, 404)
+        Checking.check_json_token(result_get, ['msg'])
 
+        print("Тестирование создания, изменения и удаления новой локации прошло успешно")
--- utils/checking.py
+++ utils/checking.py
@@ -1,13 +1,22 @@
+import json
+
 from requests import Response
 
 
-class Checking():
+class Checking:
 
     @staticmethod
-    def check_status_code(responce: Response, status_code):
-        assert status_code == responce.status_code
-        if responce.status_code == status_code:
-            print(f"Статус код = {responce.status_code}, запрос прошёл успешно")
+    def check_status_code(response: Response, status_code):
+        assert status_code == response.status_code
+        if response.status_code == status_code:
+            print(f"Статус код = {response.status_code}, запрос прошёл успешно")
         else:
-            print(f"Ошибка, статус код = {responce.status_code}")
+            print(f"Ошибка, статус код = {response.status_code}")
+
+    @staticmethod
+    def check_json_token(response: Response, expected_value):
+        token = json.loads(response.text)
+        assert list(token) == expected_value
+        print("Все поля присутствуют")
+
 
