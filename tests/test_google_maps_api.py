763b130c449784d7319c40a35297f6277e226f33
Mihail Komiser <komiser@gmail.com>
2024-11-10 16:17:29 +0300

    Создание кастомных методов

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -10,6 +10,7 @@ class TestCreatePlace:
         result_post: Response = GoogleMapsApi.create_new_place()
         Checking.check_status_code(result_post, 200)
         Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
+        Checking.check_json_value(result_post, 'status', 'OK')
 
         print("\nметод GET POST")
         result_get: Response = GoogleMapsApi.get_new_place(result_post.json()["place_id"])
@@ -17,11 +18,13 @@ class TestCreatePlace:
         Checking.check_json_token(result_get,
                                   ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
                                    'language'])
+        Checking.check_json_value(result_get, 'address', '29, side layout, cohen 09')
 
         print("\nметод PUT")
         result_put: Response = GoogleMapsApi.put_new_place(result_post.json()["place_id"])
         Checking.check_status_code(result_put, 200)
         Checking.check_json_token(result_put, ['msg'])
+        Checking.check_json_value(result_put, 'msg', 'Address successfully updated')
 
         print("\nметод GET PUT")
         result_get: Response = GoogleMapsApi.get_new_place(result_post.json()["place_id"])
@@ -29,15 +32,18 @@ class TestCreatePlace:
         Checking.check_json_token(result_get,
                                   ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
                                    'language'])
+        Checking.check_json_value(result_get, 'address', '44, Volgograd, RU')
 
         print("\nметод DELETE")
         result_delete: Response = GoogleMapsApi.delete_new_place(result_post.json()["place_id"])
         Checking.check_status_code(result_delete, 200)
         Checking.check_json_token(result_delete, ['status'])
+        Checking.check_json_value(result_delete, 'status', 'OK')
 
         print("\nметод GET DELETE")
         result_get: Response = GoogleMapsApi.get_new_place(result_post.json()["place_id"])
         Checking.check_status_code(result_get, 404)
         Checking.check_json_token(result_get, ['msg'])
+        Checking.check_json_search_word_in_value(result_get, 'msg', 'failed')
 
         print("Тестирование создания, изменения и удаления новой локации прошло успешно")
--- utils/checking.py
+++ utils/checking.py
@@ -19,4 +19,21 @@ class Checking:
         assert list(token) == expected_value
         print("Все поля присутствуют")
 
+    @staticmethod
+    def check_json_value(response: Response, field_name, expected_value):
+        check = response.json()
+        check_info = check.get(field_name)
+        assert check_info == expected_value
+        print(f"Значение поля {field_name} = {check_info}. Верно!")
+
+    @staticmethod
+    def check_json_search_word_in_value(response: Response, field_name, search_word):
+        check = response.json()
+        check_info = check.get(field_name)
+        if search_word in check_info:
+            print(f"Слово '{search_word}' найдено в поле {field_name}")
+        else:
+            print(f"Слово '{search_word}' не найдено в поле {field_name}")
+
+
 
