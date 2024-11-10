bfef8f42b8d5af1292b104f194248675cfdb15d5
Mihail Komiser <komiser@gmail.com>
2024-11-10 18:03:37 +0300

    Создание кастомных методов

--- utils/checking.py
+++ utils/checking.py
@@ -8,7 +8,7 @@ class Checking:
     # метод для проверки статус кода
     @staticmethod
     def check_status_code(response: Response, status_code):
-        assert status_code == response.status_code
+        assert status_code == response.status_code, "Ожидаемый статус код не получен"
         if response.status_code == status_code:
             print(f"Статус код = {response.status_code}, запрос прошёл успешно")
         else:
@@ -18,7 +18,7 @@ class Checking:
     @staticmethod
     def check_json_token(response: Response, expected_value):
         token = json.loads(response.text)
-        assert list(token) == expected_value
+        assert list(token) == expected_value, "Ошибка! В ответе отсутствуют обязательные ключи"
         print("Все поля присутствуют")
 
     # метод для проверки соответствия содержимого в поле ответа
@@ -26,7 +26,7 @@ class Checking:
     def check_json_value(response: Response, field_name, expected_value):
         check = response.json()
         check_info = check.get(field_name)
-        assert check_info == expected_value
+        assert check_info == expected_value, f"Ошибка! Значение поля '{field_name}' не соответствует '{expected_value}'"
         print(f"Значение поля {field_name} = {check_info}. Верно!")
 
     # метод для проверки наличия слова в ответе
